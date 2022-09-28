1af290de52b9f9dfd893d5c1493af9f67a497331
serhatbek <serhat.bekk@gmail.com>
2022-09-28 13:05:04 +0300

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,20 @@
+import { Link } from 'react-router-dom';
 import './Header.scss';
+import user from '../../images/user.png';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className='header'>
+      <div className='container'>
+        <Link>
+          <span>Movie App</span>
+        </Link>
+        <div className='user-img'>
+          <img src={user} alt='user' />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
